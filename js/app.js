e128045d014dba7384dbbc388a84eb743fada4f2
Nicolas Powell <nicopowell30@gmail.com>
2023-04-22 14:51:50 -0300

    agregue al funcion de buscador en el index

--- js/app.js
+++ js/app.js
@@ -4,19 +4,42 @@ cambiarTema(temaConfigurado);
 
 let btnThemeDark = document.querySelector("#btnThemeDark");
 let btnThemeLight = document.querySelector("#btnThemeLight");
+let buscador = document.querySelector("input");
 
+buscador.addEventListener("keyup", filtrar);
 btnThemeDark.addEventListener("click", () => cambiarTema("dark"));
 btnThemeLight.addEventListener("click", () => cambiarTema("light"));
 
 function cambiarTema(color) {
-  console.log("Cambiando Tema")
   document.querySelector("html").setAttribute("data-bs-theme", color);
-  //guardar en localstorage
   localStorage.setItem("tema", JSON.stringify(color));
-  //actualizar el icono
+
   if (color === "dark") {
     iconoTheme.className = "bi bi-moon-stars-fill";
   } else {
     iconoTheme.className = "bi bi-sun-fill";
+  }
+}
+
+function filtrar() {
+  let busqueda = buscador.value.toLowerCase();
+  let resultados = 0;
+  let articulos = document.querySelectorAll(".articulos");
+  let sinResultados = document.getElementById('mensajeError');
+
+  articulos.forEach((articulo) => {
+    let titulo = articulo.querySelector(".card-title").textContent.toLowerCase();
+    if (titulo.includes(busqueda)) {
+      articulo.style.display = "block";
+      resultados++;
+    } else {
+      articulo.style.display = "none";
+    }
+  });
+
+  if (resultados === 0 && sinResultados.className === "d-none") {
+    sinResultados.className = "text-center";
+  } else if (resultados > 0 && sinResultados.className === "text-center") {
+    sinResultados.className ="d-none";
   }
 }
