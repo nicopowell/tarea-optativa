ab0edb5e3b31f1d1fb8a2037cec437c22c1484cd
Nicolas Powell <nicopowell30@gmail.com>
2023-04-25 20:46:50 -0300

    terminé la seccion en donde se pueden agregar los comentarios, junto con un formulario para añadir un comentario

--- js/app.js
+++ js/app.js
@@ -5,10 +5,12 @@ cambiarTema(temaConfigurado);
 let btnThemeDark = document.querySelector("#btnThemeDark");
 let btnThemeLight = document.querySelector("#btnThemeLight");
 
-
+let contenedorComentarios = document.getElementById("contenedorComentarios")
+let formularioComentario = document.getElementById("formularioComentario")
 
 btnThemeDark.addEventListener("click", () => cambiarTema("dark"));
 btnThemeLight.addEventListener("click", () => cambiarTema("light"));
+formularioComentario.addEventListener("submit", agregarComentario)
 
 function cambiarTema(color) {
   document.querySelector("html").setAttribute("data-bs-theme", color);
@@ -19,5 +21,20 @@ function cambiarTema(color) {
   } else {
     iconoTheme.className = "bi bi-sun-fill";
   }
+}
+
+function agregarComentario(e){
+  e.preventDefault();
+  let inputNombre = document.getElementById("nombre").value;
+  let inputComentario = document.getElementById("comentario").value;
+
+  let comentario = document.createElement("li")
+  comentario.className = "list-group-item d-flex align-items-center"
+  comentario.innerHTML = `<i class="bi bi-person-circle fs-1 me-3"></i>
+  <div>
+    <p class="fw-bold m-0">${inputNombre}</p>
+    <p class="m-0 lead fs-6">${inputComentario}</p>
+  </div>`
+  contenedorComentarios.appendChild(comentario)
 }
 
